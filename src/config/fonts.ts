266d0275ad31e4f0baaaa0627b94853ad0dfbf68
thexeromin <thexeromin@gmail.com>
2025-02-11 09:25:33 +0530

    add: latin subset

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -2,5 +2,6 @@ import { Poppins } from "next/font/google";
 
 export const poppins = Poppins({
   weight: ["100", "200", "300", "400", "600", "700"],
+  subsets: ["latin"],
   variable: "--font-poppins"
 });
